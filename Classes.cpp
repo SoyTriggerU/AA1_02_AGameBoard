3539542df6890f3bdb2bf40333958f01b7f36a20
Julsi06 <julia.sanchez@enti.cat>
2025-03-15 17:08:28 +0100

    enum and struct created

---
+++ Classes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+enum class PlayerMovement {
+	UP,
+	DOWN,
+	LEFT,
+	RIGTH
+};
+
+struct Player {
+	int x; // Inidicates the player's position in x coordinates
+	int y; // Inidicates the player's position in y coordinates
+	int score;
+};
